678fe8c981b71d95b42e401850d824f4f7c17c17
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-09-28 16:10:35 +0300

    Start adding UI application with Iced

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+pub mod ui;
+
+use iced::widget::{button, text};
+use iced::Element;
+
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", update, view)
+}
+
+fn update(counter: &mut u64, message: Message) {
+    match message {
+        Message::Increment => *counter += 1,
+    }
+}
+
+fn view(counter: &u64) -> Element<Message> {
+    button(text(counter)).on_press(Message::Increment).into()
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
 }
