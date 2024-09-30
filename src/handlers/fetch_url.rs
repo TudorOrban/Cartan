3cc661b5cd13f0b481537bd22a71677ef0c87a58
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-09-30 16:13:45 +0300

    Switch from Reqwest to surf and async-std

--- src/handlers/fetch_url.rs
+++ src/handlers/fetch_url.rs
@@ -4,11 +4,10 @@ pub async fn fetch_url(url: String) -> Result<String, String> {
     if url.is_empty() {
         return Ok(String::from(""));
     }
-
-    reqwest::get(&url)
-        .await
-        .map_err(|e| e.to_string())?
-        .text()
-        .await
-        .map_err(|e| e.to_string())
+    
+    
+    match surf::get(url).recv_string().await {
+        Ok(text) => Ok(text),
+        Err(e) => Err(e.to_string()),
+    }
 }
